297de2bf31d71f282b5029761a08f4d557e5917e
Yaroslav <ufamoonguard@gmail.com>
2018-05-21 19:36:59 +0500

    Added player controller

---
+++ Laser Defender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+}
---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10;
+    public float Padding = 1f;
+
+    [SerializeField]
+    float xMax, xMin;
+    
+	void Start ()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xMin = leftmost.x + Padding;
+        xMax = rightmost.x - Padding;
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.position += new Vector3(-Speed * Time.deltaTime, 0f, 0f);
+            transform.position += Vector3.left * Speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.position += new Vector3(Speed * Time.deltaTime, 0f, 0f);
+            transform.position += Vector3.right * Speed * Time.deltaTime;
+        }
+
+        float clampX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        transform.position = new Vector3(clampX, transform.position.y, transform.position.z);
+    }
+}
